3e19c2632afdb5ee57f248fb9a47807472182de5
Riyaz <riyazdf@gmail.com>
2013-04-27 15:45:05 -0700

    csv_read takes string

--- parser.py
+++ parser.py
@@ -2,19 +2,23 @@ import datetime
 import csv
 import json
 
-csv_file = "sample.csv"
-data = {}
-data["readings"] = []
+csv_file = "massive file stringggggg"
 
 
 def csv_read(csv_file):
-    cr = csv.reader(open(csv_file,"rb"))
-    for row in cr:
 
+    data = {}
+    data["readings"] = []
+    rows = csv_file.split("\n")
+
+    for row in rows:
+
+        row = row.split(",")
+        
         if row and row[0].lower() in ["name", "address", "account number"]:
             data[row[0].lower()] = row[1]
             continue
-        
+
         if row and row[0].lower() == "type":
             continue
 
@@ -24,8 +28,8 @@ def csv_read(csv_file):
             reading_dict["start"] = row[2]
             reading_dict["end"] = row[3]
             reading_dict["kWh"] = float(row[4])
-            reading_dict["type"] = row[0]
-            
+            reading_dict["type"] = row[0]     
+
             month, day, year = row[1].split("/")
             hour, minute = row[2].split(":")
 
@@ -34,5 +38,3 @@ def csv_read(csv_file):
             data["readings"].append(reading_dict)
 
     return json.dumps(data)
-
-print(csv_read(csv_file))
