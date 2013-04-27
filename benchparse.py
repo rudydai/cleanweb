cf088950ccfcd889ce08cc6d9f4e66617118519e
Rudy Dai <rudydai@gmail.com>
2013-04-27 15:09:48 -0700

    added bench data

---
+++ benchparse.py
@@ -0,0 +1,33 @@
+import json
+import time
+import sys
+
+def parse_input(filein, fileout):
+    readin = open(filein, 'r')
+    writeout = open(fileout, 'w')
+    measurements = []
+    for line in readin:
+        startime = 0
+        entries = line.split(',')
+        if len(entries) != 26:
+            print "incorrectly formatted input: "+line
+            break
+        date = entries[1].strip()
+        entries = entries[2:]
+        for meas in entries:
+            epochtime = int(time.mktime(time.strptime(date+" "+str(startime)+":00", "%m/%d/%Y %H:%M")))
+            start = str(startime)+":00"
+            end = str((startime+1)%24)+":00"
+            measure = float(meas)
+            entry = {"time": epochtime, "date":date, "start":start, "end":end, "value": measure}
+            measurements.append(entry)
+            startime += 1
+    writeout.write(json.dumps({"readings": measurements, "type": "Electricity", "units": "kWh"}, sort_keys=True, indent=4, separators=(",", ": ")))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage is: python benchparse.py filein fileout")
+    else:
+        parse_input(sys.argv[1], sys.argv[2])
+
+
