d3c7b98f6a81caaada26a179d2b642a17314116b
Riyaz <riyazdf@gmail.com>
2013-04-27 17:07:53 -0700

    added avg weekday/end energy

--- parser.py
+++ parser.py
@@ -10,6 +10,9 @@ def csv_read(csv_file):
     data = {}
     data["readings"] = []
     rows = csv_file.split("\n")
+    
+    weekend_count, weekend_energy = 0, 0
+    weekday_count, weekday_energy = 0, 0
 
     for row in rows:
 
@@ -32,9 +35,20 @@ def csv_read(csv_file):
 
             month, day, year = row[1].split("/")
             hour, minute = row[2].split(":")
+            date = datetime.datetime(int(year), int(month), int(day), int(hour), int(minute))
+            
+            if date.weekday() > 4:
+                weekend_count += 1
+                weekend_energy += float(row[4])
+            else:
+                weekday_count += 1
+                weekday_energy += float(row[4])
 
-            reading_dict["time"] = int(datetime.datetime(int(year), int(month), int(day), int(hour), int(minute)).strftime('%s'))
+            reading_dict["time"] = int(date.strftime('%s'))
 
             data["readings"].append(reading_dict)
+    
+    data["avg_weekday_energy"] = float(weekday_energy)/weekday_count
+    data["avg_weekend_energy"] = float(weekend_energy)/weekend_count
 
     return json.dumps(data)
